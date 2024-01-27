fb5f1446485a7cd81d85ff731fef6cdc4501bd6a
acpass <luoyuchen@nuaa.edu.cn>
2024-01-27 21:02:26 +0800

    commit

--- exception2.cpp
+++ exception2.cpp
@@ -10,9 +10,12 @@ int main() {
   int arr[3] = {1, 2, 3};
   for (int i = 0; i <= 5; i++) {
     try{
+      if(i>(v.size()-1)||i>(sizeof(arr)-1))
+        throw out_of_range("big index");
       cout << v[i] << arr[i] << endl;
     } catch (out_of_range err) {
       cout << err.what() << '\n' << "you are out of range!" << endl;
+      break;
     }
   }
   cout << "terminal" << endl;
