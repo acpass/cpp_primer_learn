3e42fa8c7dc6dd8006b37e91acfb263a91947870
acpass <luoyuchen@nuaa.edu.cn>
2024-02-10 19:15:57 +0800

    test aggregate class and its initializing

---
+++ include/aggregate_class.hpp
@@ -0,0 +1,16 @@
+#ifndef _AGGREGATE_CLASS_HPP_
+#define _AGGREGATE_CLASS_HPP_
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+class test_class
+{
+public:
+  int a;
+  int b;
+  std::string str;
+};
+
+#endif
