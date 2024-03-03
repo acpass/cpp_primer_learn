92407ff89d0d5ee9348279b3bddd835beb1af741
acpass <luoyuchen@nuaa.edu.cn>
2024-03-03 14:36:21 +0000

    try kinds of iterators

--- src/iterator.cpp
+++ src/iterator.cpp
@@ -29,7 +29,35 @@ int main()
   }
   else
   {
+    cout << "failed to open file";
+  }
+  cout << endl;
+
+  ifstream fd_in("../test_file/file1");
+  if (fd_in.is_open())
+  {
+    istream_iterator<string> inf_iter1(fd_in), inf_iter2(fd_in), eof;
+    ostream_iterator<string> os_iter(cout, " ");
+
+    while (inf_iter1 != eof && inf_iter2 != eof)
+    {
+      *os_iter = *inf_iter1++;
+      *os_iter = *inf_iter2++;
+      //dereference just return a element bound to the iterator (read previously), it is the ++ operator who really read a element from the stream
+      //except for the first read, which should be done when initializing but can be delay to sync different iterator bound to the same stream (lazy read)
+    }
+    cout << "$eof$";
+    cout << endl;
+  }
+  else
+  {
     cout << "failed to open file" << endl;
   }
+  fd_in.close();
+
+  string s2{"12,321,12,5325,234,6436,dfsd"};
+  auto last_word = find(s2.crbegin(), s2.crend(), ',');
+  cout << string(last_word.base(), s2.crbegin().base()) << endl; //base() return the next iterator for a "normal" last_word
+
   return 0;
 }
